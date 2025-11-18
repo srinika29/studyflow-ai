245f66f259e9fe25e290da1eac2127f3e7b08221
Srinika Nulu <srinika@Srinikas-MacBook-Air.local>
2025-11-18 12:37:48 +0530

    Use native fetch instead of node-fetch

--- netlify/functions/claude.js
+++ netlify/functions/claude.js
@@ -1,5 +1,3 @@
-const fetch = require('node-fetch');
-
 exports.handler = async (event) => {
   // Only allow POST
   if (event.httpMethod !== 'POST') {
@@ -39,4 +37,3 @@ exports.handler = async (event) => {
     };
   }
 };
-
